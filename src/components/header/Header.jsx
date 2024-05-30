f4e989e737b95ff9fdecd2374182f6ab30e406a6
malik talha <tm2797169@gmail.com>
2024-05-31 02:33:17 +0500

    img update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -13,7 +13,9 @@ const Header = () => {
         <CTA />
         <HeaderSocials />
 
-        <div className="me">{/* <img src="../../../public/Me.jpg" alt="me" /> */}</div>
+        <div className="me">
+          <img src="../../../public/Me.jpg" alt="me" />
+        </div>
         <a href="#contact" className="scroll__down">
           Scroll Down
         </a>
